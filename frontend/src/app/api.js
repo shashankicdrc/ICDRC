d917249385b91a8d9e2211c50968c4f725d510ea
abhishekdev45 <156638090+AbhishekWebdesys@users.noreply.github.com>
2024-02-21 17:33:27 +0530

    Update api.js

--- frontend/src/app/api.js
+++ frontend/src/app/api.js
@@ -1,21 +1,19 @@
 // export const url = 'http://localhost:5000'
-export const url = 'http://193.203.162.161:4007'
+// export const url = 'http://193.203.162.161:4007'
+
+export const url = "https://api.icdrc.in";
 // export const url = 'https://www.backend.webdesys.com'
 
-
-
 export const UserAuthAPI = {
-   
-    // userAccountOTPVerification_API:
-    //   BASE_URL + "/api/v1/userAccountOTPVerification",
-   
-   
-    // changeUserAccountStatus_API: BASE_URL + "/api/v1/changeUserAccountStatus",
-  
-    // userLogin_API: BASE_URL + "/api/v1/userLogin",
-    userUpdatePassword_API: url + "/api/v1/userUpdatePassword",
-    // userUpdateProfile_API: BASE_URL + "/api/v1/userUpdateProfile",
-    // userGetMyProfile_API: BASE_URL + "/api/v1/userGetMyProfile",
-    userSendOTP_API: url + "/api/v1/userSendOTP",
-    userVerifyOTP_API: url + "/api/v1/userVerifyOTP",
-  };
+  // userAccountOTPVerification_API:
+  //   BASE_URL + "/api/v1/userAccountOTPVerification",
+
+  // changeUserAccountStatus_API: BASE_URL + "/api/v1/changeUserAccountStatus",
+
+  // userLogin_API: BASE_URL + "/api/v1/userLogin",
+  userUpdatePassword_API: url + "/api/v1/userUpdatePassword",
+  // userUpdateProfile_API: BASE_URL + "/api/v1/userUpdateProfile",
+  // userGetMyProfile_API: BASE_URL + "/api/v1/userGetMyProfile",
+  userSendOTP_API: url + "/api/v1/userSendOTP",
+  userVerifyOTP_API: url + "/api/v1/userVerifyOTP",
+};
