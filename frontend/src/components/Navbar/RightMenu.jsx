c8709f2333012b869af63f802da3d830d743d080
Hayat ilyas <ihayat855@gmail.com>
2024-04-30 09:22:41 +0530

    Redesign the Navbar for mobile view

--- frontend/src/components/Navbar/RightMenu.jsx
+++ frontend/src/components/Navbar/RightMenu.jsx
@@ -1,12 +1,11 @@
 import React, { Fragment } from "react";
 import Link from "next/link";
+import CaseStatus from "../form/CaseStatus";
 
 export default function RightMenu() {
   return (
     <Fragment>
-      <button className="text-white bg-orange-500 hover:bg-orange-600 px-4 py-2 rounded-md">
-        <Link href="/casestatus">Case status</Link>
-      </button>
+      <CaseStatus />
       <button className="text-white bg-orange-500 hover:bg-orange-600 px-4 py-2 rounded-md">
         <Link href="/register">Register complaints</Link>
       </button>
--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,36 +1,22 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import Link from "next/link";
 import "../../styles/module.navbar.css";
 import PrivateHomeNavLinks from "./PrivateHomeLink";
 import { IoMdMenu } from "react-icons/io";
+import dynamic from "next/dynamic";
 import RightMenu from "./RightMenu";
+
+const MobileSideModal = dynamic(() => import("./MobileNavbarModal"));
 
 const HomeNav = () => {
   const [open, setOpen] = useState(false);
 
-  const [navbar, setNavbar] = useState(false);
-
-  const changeBackground = () => {
-    // console.log(window.scrollY)
-    if (window.scrollY >= 50) {
-      setNavbar(true);
-    } else {
-      setNavbar(false);
-    }
-  };
-
-  useEffect(() => {
-    changeBackground();
-    // adding the event when scroll change background
-    window.addEventListener("scroll", changeBackground);
-  });
+  const toggleMobileModal = useCallback((value) => setOpen(value), []);
 
   return (
     <nav
-      className={`${
-        navbar ? "bg-white" : ""
-      }  fixed top-0 w-full bg-white shadow-lg z-50 transition-all ease-in-out duration-75`}
+      className={`fixed top-0 w-full bg-white shadow-lg z-50 transition-all ease-in-out duration-75`}
     >
       <div className="flex items-center font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between h-full">
@@ -43,9 +29,12 @@ const HomeNav = () => {
               width={400}
             />
           </Link>
-          <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
-            <IoMdMenu name={`${open ? "close" : "menu"}`}></IoMdMenu>
-          </div>
+          <button
+            className={`text-3xl md:hidden ${open ? "hidden" : ""}`}
+            onClick={() => toggleMobileModal(!open)}
+          >
+            <IoMdMenu className="text-black" />
+          </button>
         </div>
         <ul className="md:flex hidden items-center gap-8 font-[Signika+Negative] z-20">
           <li>
@@ -64,7 +53,7 @@ const HomeNav = () => {
               About us
             </Link>
           </li>
-          <PrivateHomeNavLinks navbar={navbar} />
+          <PrivateHomeNavLinks />
           <li>
             <Link
               href="/partner"
@@ -82,41 +71,10 @@ const HomeNav = () => {
             </Link>
           </li>
         </ul>
-        <div className="md:block hidden">
-          <Button />
+        <div className="hidden md:flex md:item-center md:space-x-2">
+          <RightMenu />
         </div>
-        {/* Mobile nav */}
-        <ul
-          className={`
-        md:hidden  fixed w-4/5 bg-gray-100 top-0 overflow-y-auto bottom-0 py-24 pl-4
-        duration-500 ${open ? "left-0" : "left-[-100%]"}
-        `}
-        >
-          <li>
-            <Link href="/" className="py-7 px-3 inline-block">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="py-7 px-3 inline-block">
-              About us
-            </Link>
-          </li>
-          <PrivateHomeNavLinks />
-          <li>
-            <Link href="/partner" className="py-7 px-3 inline-block">
-              Partner with us
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" className="py-7 px-3 inline-block">
-              Contact us
-            </Link>
-          </li>
-          <div className="py-5">
-            <RightMenu />
-          </div>
-        </ul>
+        <MobileSideModal isOpen={open} toggleModal={toggleMobileModal} />
       </div>
     </nav>
   );
