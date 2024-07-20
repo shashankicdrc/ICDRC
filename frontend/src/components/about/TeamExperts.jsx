13f997d691e9076a87e951c9c4aaa681239e6eb8
Hayat ilyas <ihayat855@gmail.com>
2024-07-20 20:42:50 +0530

    Adding teams

--- frontend/src/components/about/TeamExperts.jsx
+++ frontend/src/components/about/TeamExperts.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
 import Image from 'next/image'
 
+const experts = [
+    {
+        name: 'NIRANJAN UPADHYAYA',
+        bio: "U.N. Niranjan is a seasoned professional with 24 years of varied experience in the automotive, general insurance, testing, inspection, and certification industries. He has over 4 years of experience in the TIC (Testing, Inspection, and Certification) industry. Niranjan has successfully led large teams comprising more than a hundred members. Currently, he serves as the Principal Surveyor (Director) at M/S Spartans Insurance Surveyors and Loss Assessors.",
+        image: '/images/team/niranjan.jpeg'
+    },
+    {
+        name: 'PRAVEEN KUMAR CHHAJED',
+        bio: "Former SR.VP & Head-Non Motor Claims, Bajaj Allianz Group, possessing over 25 years of comprehensive experience and verifiable accomplishments in claims management in the insurance sector; career underscored with a series of progressive roles in the insurance sector across the Middle East and India.",
+        image: '/images/team/praveen.jpeg'
+    },
+    {
+        name: 'NUPUR NADDA',
+        bio: "General Manager at National Insurance Company Ltd and a seasoned insurance relationship manager with 25 years of expertise.",
+        image: '/images/team/nupur.JPG'
+    }
+];
+
 const TeamExperts = () => {
     const [readMore, setReadMore] = useState({});
-    const experts = [
-        {
-            name: 'NIRANJAN UPADHYAYA',
-            bio: "Niranjan is a IRDAI licenced surveyor based at Bengaluru, Karnataka and is currently Partner at Spartan Surveyors & Loss Assessors for the last 4 years. He started his career at an automobile repair & service centre and then moved on to General Insurance company assessing motor claims growing from a Claims Executive to the Zonal Claims Manager at leading general insurance companies spanning overall 16 years.During his last stint, he also managed the role of Zonal Underwriting Manager for an Insurance Company. Niranjan is an Automobile Engineer by education from Karnataka University and a qualified Chartered Engineer.He is also an Associate member of Indian Institute of Insurance Surveyors and Loss Assessors & a Fellow of Insurance Institute of India.",
-            image: '/images/team/niranjan.jpeg'
-        },
-        //{
-        //    name: 'NIRANJAN UPADHYAYA',
-        //    bio: "Niranjan is a IRDAI licenced surveyor based at Bengaluru, Karnataka and is currently Partner at Spartan Surveyors & Loss Assessors for the last 4 years. He started his career at an automobile repair & service centre and then moved on to General Insurance company assessing motor claims growing from a Claims Executive to the Zonal Claims Manager at leading general insurance companies spanning overall 16 years.During his last stint, he also managed the role of Zonal Underwriting Manager for an Insurance Company. Niranjan is an Automobile Engineer by education from Karnataka University and a qualified Chartered Engineer.He is also an Associate member of Indian Institute of Insurance Surveyors and Loss Assessors & a Fellow of Insurance Institute of India.",
-        //    image: '/images/team/niranjan.jpeg'
-        //},
-        //{
-        //    name: 'NIRANJAN UPADHYAYA',
-        //    bio: "Niranjan is a IRDAI licenced surveyor based at Bengaluru, Karnataka and is currently Partner at Spartan Surveyors & Loss Assessors for the last 4 years. He started his career at an automobile repair & service centre and then moved on to General Insurance company assessing motor claims growing from a Claims Executive to the Zonal Claims Manager at leading general insurance companies spanning overall 16 years.During his last stint, he also managed the role of Zonal Underwriting Manager for an Insurance Company. Niranjan is an Automobile Engineer by education from Karnataka University and a qualified Chartered Engineer.He is also an Associate member of Indian Institute of Insurance Surveyors and Loss Assessors & a Fellow of Insurance Institute of India.",
-        //    image: '/images/team/niranjan.jpeg'
-        //},
-    ];
 
     const toggleReadMore = (index) => {
         setReadMore((prevState) => ({
@@ -37,9 +38,9 @@ const TeamExperts = () => {
                         <p className="text-gray-500 -translate-y-4">Meet the experts behind our success</p>
                     </div>
                 </div>
-                <ul className="flex justify-center">
+                <ul className="flex flex-wrap justify-center space-x-4">
                     {experts.map((expert, index) => (
-                        <li className="border rounded-md w-96">
+                        <li key={index} className="border rounded-md w-96 mb-4">
                             <Image
                                 src={expert.image}
                                 alt={expert.name}
@@ -48,23 +49,23 @@ const TeamExperts = () => {
                                 className="rounded-t-lg"
                             />
                             <div className="space-y-2 px-5 pt-5 pb-2">
-                                <div className="text-lg leading-6 font-medium space-y-1">
+                                <div className="text-lg leading-6 font-medium">
                                     <h3>{expert.name}</h3>
                                 </div>
                                 <p className="text-gray-500 text-justify">
-                                    {readMore[index] ? expert.bio : `${expert.bio.substring(0, 200)}...`}
-                                    <button
-                                        className="text-indigo-600 hover:text-indigo-900"
-                                        onClick={() => toggleReadMore(index)}
-                                    >
-                                        {readMore[index] ? ' Show Less' : ' Read More'}
-                                    </button>
+                                    {readMore[index] || expert.bio.length <= 150 ? expert.bio : `${expert.bio.substring(0, 150)}...`}
+                                    {expert.bio.length > 150 && (
+                                        <button
+                                            className="text-indigo-600 hover:text-indigo-900 ml-1"
+                                            onClick={() => toggleReadMore(index)}
+                                        >
+                                            {readMore[index] ? 'Show Less' : 'Read More'}
+                                        </button>
+                                    )}
                                 </p>
                             </div>
-
                         </li>
                     ))}
-
                 </ul>
             </div>
         </div>
