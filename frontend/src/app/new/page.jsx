2cfdc7507a6eb9c2813afc9d6b4445972a1fcd8c
Hayat ilyas <ihayat855@gmail.com>
2024-08-24 07:02:55 +0530

    Adding pricing page

---
+++ frontend/src/app/new/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PricingCard from '../../components/HomeComponents/PricingCard';
+
+export default function page() {
+    return (
+        <main className="my-5 mx-5">
+            <PricingCard />
+        </main>
+    );
+}
