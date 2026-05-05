d91a6f01c31477dd7341b449ecbe5e5843c3fd12
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-05-05 22:27:19 +0530

    remove: Subscription button and Mediation button

--- frontend/src/components/Navbar/RightMenu.jsx
+++ frontend/src/components/Navbar/RightMenu.jsx
@@ -7,13 +7,7 @@ export default function RightMenu() {
     return (
         <Fragment>
             <Button asChild>
-                <Link href="/dashboard/mediation">Mediation</Link>
-            </Button>
-            <Button asChild>
                 <Link href="/dashboard/register">Register Complaints</Link>
-            </Button>
-            <Button asChild>
-                <Link href="/#subscription">Subscribe Now</Link>
             </Button>
         </Fragment>
     );
