57ff22fd33a1d433d7b5afb2db9bdb628dcea99f
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-05-06 01:42:02 +0530

    remove: payment service function

--- frontend/src/externalAPI/paymentService.js
+++ frontend/src/externalAPI/paymentService.js
@@ -29,21 +29,3 @@ export const userPaymentHistory = async (token) => {
         return { message, data };
     }
 };
-
-// Mediation Payment 
-export const initiatePayment = async (token, values) => {
-    const result = await fetch(`${BASE_URL}/api/mediation-payment/initiate`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ ...values }),
-    });
-    const { message, statusCode, status, data } = await result.json();
-    if (httpStatusCode.OK !== statusCode && httpStatus.SUCCESS !== status) {
-        return { error: message };
-    } else {
-        return { message, data };
-    }
-};
