4122afe219f443cc891d621bf83fe90a22f10db8
Ajay Pandey <ajaypandeyy1123@gmail.com>
2026-05-06 01:36:07 +0530

    feat: new button for become mediator

--- frontend/src/components/Navbar/RightMenu.jsx
+++ frontend/src/components/Navbar/RightMenu.jsx
@@ -9,6 +9,9 @@ export default function RightMenu() {
             <Button asChild>
                 <Link href="/dashboard/register">Register Complaints</Link>
             </Button>
+            <Button asChild>
+                <Link href="/become-mediator">Become a Mediator</Link>
+            </Button>
         </Fragment>
     );
 }
